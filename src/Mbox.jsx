98b2e26a176d4b75d4c6e4b36c7595236c227d7a
hamsarajpitani <pitanihamsaraj@gmail.com>
2021-03-09 22:56:08 +0530

    rating added

--- src/Mbox.jsx
+++ src/Mbox.jsx
@@ -5,6 +5,10 @@ const Mbox = ({ title, fetchURL, isLarge }) => {
   const baseUrl = "https://image.tmdb.org/t/p/original/";
   const [Movie, setMovie] = useState([]);
 
+  function truncate(string, n) {
+    return string?.length > n ? string.substring(0, n - 1) + "...." : string;
+  }
+
   useEffect(() => {
     const getdata = async () => {
       await axios.get(fetchURL).then(({ data }) => {
@@ -24,28 +28,30 @@ const Mbox = ({ title, fetchURL, isLarge }) => {
         <h1>{title}</h1>
       </div>
 
-      <div className="movie__posters my-4">
+      <div className="movie__posters__div my-4">
         {Movie.map((movie) => {
           return (
-            //!! fix deadlink in case any image icon instead of actual image
+            // //!! fix deadlink in case any image icon instead of actual image
             // (isLarge && movie.poster_path) ||
             // (!isLarge && movie.backdrop_path && (
-            <>
+              
+            <div id="relative">
               <img
-                className={`card-img-top movie__poster
-               ${isLarge && "movie__poster__large"}`}
+              id="img"
+                className={`movie__poster__img
+               ${isLarge && "movie__poster__img__large"}`}
                 key={movie.id}
                 src={`${baseUrl}${
                   isLarge ? movie.poster_path : movie.backdrop_path
                 }`}
                 alt={movie.name}
               />
-
-              {/* <h1 className="name">
-                {movie.title ? movie.title : movie.original_title}
-               
-              </h1> */}
-            </>
+              <div id="hover__info" className="hover__info">
+              <h1 className="name">{movie.name || movie.title}</h1>
+              <p className="overview">{truncate(movie.overview, 60)}</p>
+              <p className="vote">{movie.vote_average}</p>
+              </div>
+            </div>
           );
         })}
       </div>
